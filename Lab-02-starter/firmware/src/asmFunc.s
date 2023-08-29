a961250d0e0cb8f316b7e797790d9744fc12d3aa
rulatu <142952003+rulatu@users.noreply.github.com>
2023-08-29 12:46:05 -0700

    Added an ADD instruction to asmFunc.s

--- Lab-02-starter/firmware/src/asmFunc.s
+++ Lab-02-starter/firmware/src/asmFunc.s
@@ -11,7 +11,7 @@
 /* create a string */
     
 /*** STUDENTS: HINT: this is the string that the C code prints out!  **/
-nameStr: .asciz "Hello. My name is Inigo Montoya."  
+nameStr: .asciz "Hello. My name is Rula Al Turaihi."  
  
 /* initialize a global variable that C can access to print the nameStr */
 .global nameStrPtr
@@ -54,8 +54,9 @@ asmFunc:
 
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
-
     
+    add r0, r1, r0
+   
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
     
     /* restore the caller's registers, as required by the 
